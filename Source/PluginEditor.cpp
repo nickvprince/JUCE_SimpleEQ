e3ab79e12183239727f266581db89ac83c0d0f9a
nickvprince <nickvprince@gmail.com>
2022-10-26 01:49:14 -0400

    Code Cleanup
    Removed Commented Code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -232,8 +232,7 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
 	g.drawImage(background, getLocalBounds().toFloat());
 
-	//auto responseArea = getLocalBounds();
-	auto responseArea = getAnalysisArea(); //getRenderArea();
+	auto responseArea = getAnalysisArea();
 
 	auto width = responseArea.getWidth();
 
@@ -327,10 +326,8 @@ void ResponseCurveComponent::resized() {
 
 
 	g.setColour(Colours::dimgrey);
-	//for (auto f : freqs) {
+
 	for (auto x : xs) {
-		//auto normX = mapFromLog10(f, 20.f, 20000.f);
-		//g.drawVerticalLine(getWidth() * normX, 0.f, getHeight());
 		g.drawVerticalLine(x, top, bottom);
 	}
 
@@ -340,12 +337,9 @@ void ResponseCurveComponent::resized() {
 
 	for (auto gDb : gain) {
 		auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
-		//g.drawHorizontalLine(y, 0, getWidth());
 		g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey);
 		g.drawHorizontalLine(y, left, right);
 	}
-
-	//g.drawRect(getAnalysisArea());
 
 	g.setColour(Colours::lightgrey);
 	const int fontHeight = 10;
@@ -405,10 +399,6 @@ void ResponseCurveComponent::resized() {
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea() {
 
 	auto bounds = getLocalBounds();
-	
-	//bounds.reduce(10, //JUCE_LIVE_CONSTANT(5),
-	//				8 //JUCE_LIVE_CONSTANT(5)
-	//				);
 
 	bounds.removeFromTop(12);
 	bounds.removeFromBottom(2);
